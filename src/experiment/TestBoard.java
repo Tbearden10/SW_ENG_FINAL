f416feec9ec3192cefe231853f7c49af30b2b812
Tanner Bearden <tannerbeardens@gmail.com>
2024-02-22 18:34:25 -0700

    Small changes for C13A

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,13 +1,23 @@
 package experiment;
 
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class TestBoard {
+
+    TestBoardCell[][] board;
+
+    Set<TestBoardCell> targets;
 
     public TestBoard() {}
 
     public void calcTargets(TestBoardCell startCell, int pathLength) {
         
+    }
+
+    public TestBoard getCell(int row, int col) {
+        return null;
     }
 
     public Set<TestBoardCell> getTargets() {
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,19 +1,28 @@
 package experiment;
 
 import java.util.Set;
+import java.util.TreeSet;
 public class TestBoardCell {
 
     private int row;
     private int col;
+    private Set<TestBoardCell> adjList;
+    private boolean isRoom;
+    private boolean isOccupied;
 
 
     public TestBoardCell(int row, int col) {
         this.row = row;
         this.col = col;
+        adjList = new TreeSet<TestBoardCell>();
     }
+
 
     public Set<TestBoardCell> getAdjList() {
         return null;
     }
+
+    
+
 
 }
