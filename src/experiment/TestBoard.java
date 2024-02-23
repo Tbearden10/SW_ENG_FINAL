033984c0f42654ed608006bd34c2eca738532f00
Tanner Bearden <tannerbeardens@gmail.com>
2024-02-22 17:17:37 -0700

    Setup TestBoard and TestBoardCell classes C13A

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,18 @@
 package experiment;
 
+import java.util.Set;
+
 public class TestBoard {
+
+    public TestBoard() {}
+
+    public void calcTargets(TestBoardCell startCell, int pathLength) {
+        
+    }
+
+    public Set<TestBoardCell> getTargets() {
+        return null;
+    }
+
     
 }
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,19 @@
 package experiment;
 
+import java.util.Set;
 public class TestBoardCell {
-    
+
+    private int row;
+    private int col;
+
+
+    public TestBoardCell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Set<TestBoardCell> getAdjList() {
+        return null;
+    }
+
 }
