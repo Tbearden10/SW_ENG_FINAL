08e8001b0c1d2f9464f03ec275e70caf34d6aff4
Tanner Bearden <tannerbeardens@gmail.com>
2024-04-04 16:09:45 -0600

    test

---
+++ src/clueGame/GameInformationPanel.java
@@ -0,0 +1,73 @@
+/*
+ * Authors Brayden Clark and Tanner Bearden
+ * Date: 4/4/2024
+ * 
+ * Collaborators: None
+ * 
+ * 
+ * This class is used to setup and display buttons and game information
+ */
+package clueGame;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class GameInformationPanel extends JPanel {
+
+
+    JPanel peoplePanel;
+    JPanel roomPanel;
+    JPanel weaponPanel;
+    
+    
+    
+    public GameInformationPanel() {
+        super();
+        initializePanels();
+        updatePanels();
+    }
+
+    /**
+     * Initializes the panels for the game control panel
+     */
+    private void initializePanels() {
+        setLayout(new GridLayout(1,1));
+
+
+    }
+
+    private void updatePanel(JPanel panel, CardType type) {
+        panel.removeAll();
+        // rebuid contents and add it pack to parent
+
+        add(panel);
+    }
+
+    private void updatePanels() {
+        // update the known cards
+        updatePanel(peoplePanel, CardType.PERSON);
+        updatePanel(roomPanel, CardType.ROOM);
+        updatePanel(weaponPanel, CardType.WEAPON);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        GameInformationPanel panel = new GameInformationPanel();
+        JFrame frame = new JFrame();
+        frame.setContentPane(panel);
+        frame.setSize(160, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        // Test filling in the data
+    }
+
+}
