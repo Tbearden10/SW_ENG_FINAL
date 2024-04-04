d4573e68d3a2397ac896782322360ddaad68f585
Tanner Bearden <tannerbeardens@gmail.com>
2024-04-04 16:56:32 -0600

    Working on cards panel

--- src/clueGame/GameInformationPanel.java
+++ src/clueGame/GameInformationPanel.java
@@ -10,36 +10,44 @@
 package clueGame;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.Set;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
+import javax.swing.border.Border;
+import java.util.ArrayList;
 
 public class GameInformationPanel extends JPanel {
 
 
-    JPanel peoplePanel;
-    JPanel roomPanel;
-    JPanel weaponPanel;
+    JPanel peoplePanel = new JPanel(new GridLayout(0, 1));
+    JPanel roomPanel = new JPanel(new GridLayout(0,1));
+    JPanel weaponPanel = new JPanel(new GridLayout(0,1));
     
     
     
     public GameInformationPanel() {
         super();
         initializePanels();
-        //updatePanels();
     }
 
     /**
      * Initializes the panels for the game control panel
      */
     private void initializePanels() {
-        setLayout(new GridLayout(1,1));
+        
+        // set layout for the panel
+        setLayout(new GridLayout(3, 1));
 
-
+        // Set border titles
+        setBorder(BorderFactory.createTitledBorder("Known Cards"));
+        peoplePanel.setBorder(BorderFactory.createTitledBorder("People"));
+        roomPanel.setBorder(BorderFactory.createTitledBorder("Rooms"));
+        weaponPanel.setBorder(BorderFactory.createTitledBorder("Weapons"));
 
         add(peoplePanel);
         add(roomPanel);
@@ -47,19 +55,45 @@ public class GameInformationPanel extends JPanel {
 
     }
 
-    private void updatePanel(JPanel panel, CardType card) {
-        
-        // rebuid contents and add it pack to parent
-        
+    private void updatePanel(JPanel panel, Player player, CardType type) {
+        panel.removeAll();
 
+        ArrayList<Card> cards = player.getCards();
+        Set<Card> seenCards = player.getSeenCards();
+
+        // rebuid contents and add it pack to parent
+        panel.add(new JLabel("In Hand: "));
+
+        // loop through cards in hand
+        for (Card card : cards) {
+            if (card.getCardType() == type) {
+                JTextField textField = new JTextField(card.getCardName());
+                textField.setEditable(false);
+                panel.add(textField);
+            }
+        }
+
+        panel.add(new JLabel("Seen: "));
+
+        // loop through seen cards
+        for (Card card : seenCards) {
+            if (card.getCardType() == type) {
+                JTextField textField = new JTextField(card.getCardName());
+                textField.setEditable(false);
+                panel.add(textField);
+            }
+        }
         
+        
+        add(panel);
     }
 
-    private void updatePanels() {
+    public void updatePanels(Player player) {
+
         // update the known cards
-        updatePanel(peoplePanel, CardType.PERSON);
-        updatePanel(roomPanel, CardType.ROOM);
-        updatePanel(weaponPanel, CardType.WEAPON);
+        updatePanel(peoplePanel, player, CardType.PERSON);
+        updatePanel(roomPanel, player, CardType.ROOM);
+        updatePanel(weaponPanel, player, CardType.WEAPON);
     }
 
 
@@ -74,6 +108,14 @@ public class GameInformationPanel extends JPanel {
         frame.setVisible(true);
 
         // Test filling in the data
+        Board board = Board.getInstance();
+        board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+        board.initialize();
+        board.deal();
+
+        Player testPlayer = board.getPlayers().get(0);
+        
+        panel.updatePanels(testPlayer);
     }
 
 }
